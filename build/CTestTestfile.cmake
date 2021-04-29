6be26e7e688a08b178aeed753a1f123c2e9eb8a2
Michal Heit <michalnt98@gmail.com>
2021-04-29 14:48:13 +0200

    usuniecie build, devel, .catkin_workspace z gita i dodanie .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wika/TSWR_projekt/src
-# Build directory: /home/wika/TSWR_projekt/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
