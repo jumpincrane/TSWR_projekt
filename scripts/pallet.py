6f3c8bc4750b7ff751ca8e9ab2f7b0fed4be12cf
Michał Heit <michalnt98@gmail.com>
2021-05-07 20:19:20 +0200

    sub process added

--- scripts/pallet.py
+++ scripts/pallet.py
@@ -1,11 +1,16 @@
 from statemachine import StateMachine, State, Transition
-from statemachine.mixins import MachineMixin
 
 # Supervisor process
 # IDLE
 # A = waiting for empty pallet to arrive
 # B = palletizing process
 # C = waiting for full pallet to exit
+
+# Sub process
+# IDLE = waiting for palletization process to start
+# A = waiting for product to arrive
+# B = product is ready to hold
+# C = product is up
 
 # Palletization Process
 # IDLE = waiting for ready product to hold
@@ -17,6 +22,13 @@ from statemachine.mixins import MachineMixin
 # F = product is released
 
 supervisor_options = [
+    {"name": "IDLE", "initial": True, "value": "idle"},
+    {"name": "A", "initial": False, "value": "a"},
+    {"name": "B", "initial": False, "value": "b"},
+    {"name": "C", "initial": False, "value": "c"}
+]
+
+sub_options = [
     {"name": "IDLE", "initial": True, "value": "idle"},
     {"name": "A", "initial": False, "value": "a"},
     {"name": "B", "initial": False, "value": "b"},
@@ -35,6 +47,7 @@ pallet_p_options = [
 # create State objects for a master
 pallet_p_states = [State(**opt) for opt in pallet_p_options]
 supervisor_states = [State(**opt) for opt in supervisor_options]
+sub_states = [State(**opt) for opt in sub_options]
 
 pallet_p_form_to = [
     [0, [1]],
@@ -53,8 +66,16 @@ supervisor_form_to = [
     [3, [0]]
 ]
 
+sub_form_to = [
+    [0, [1]],
+    [1, [2]],
+    [2, [3]],
+    [3, [0, 1]]
+]
+
 pallet_p_transitions = {}
 supervisor_transitions = {}
+sub_transitions = {}
 
 for indices in pallet_p_form_to:
     from_idx, to_idx_tuple = indices  # unpack list of two elements into separate from_idx and to_idx_tuple
@@ -72,6 +93,14 @@ for indices in supervisor_form_to:
         transition = Transition(supervisor_states[from_idx], supervisor_states[to_idx], identifier=op_identifier)
         supervisor_transitions[op_identifier] = transition
         supervisor_states[from_idx].transitions.append(transition)
+
+for indices in sub_form_to:
+    from_idx, to_idx_tuple = indices  # unpack list of two elements into separate from_idx and to_idx_tuple
+    for to_idx in to_idx_tuple:  # iterate over destinations from a source state
+        op_identifier = "m_{}_{}".format(from_idx, to_idx)  # parametrize identifier of a transition
+        transition = Transition(sub_states[from_idx], sub_states[to_idx], identifier=op_identifier)
+        sub_transitions[op_identifier] = transition
+        sub_states[from_idx].transitions.append(transition)
 
 
 class Gen(StateMachine):
@@ -109,12 +138,12 @@ class Gen(StateMachine):
 def main():
     pallet_process = Gen(pallet_p_states, pallet_p_transitions)
     supervisor = Gen(supervisor_states, supervisor_transitions)
+    sub = Gen(sub_states, sub_transitions)
     print(pallet_process)
     pallet_path = ["m_0_1", "m_1_2", "m_2_3", "m_3_2"]
     supervisor_path = ["m_0_1", "m_1_2", "m_2_3", "m_3_0"]
     pallet_paths = [pallet_path]
     supervisor_paths = [supervisor_path]
-    
 
 
 
